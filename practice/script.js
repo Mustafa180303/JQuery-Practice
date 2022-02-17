4bd73c96b2c4cc6a049e8e36f37941cb41a08102
Mustafa180303 <96572226+Mustafa180303@users.noreply.github.com>
2022-02-17 23:56:14 +0600

    Add files via upload

---
+++ practice/script.js
@@ -0,0 +1,21 @@
+
+
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function terms_changed(termsCheckBox){
+    //If the checkbox has been checked
+    if(termsCheckBox.checked){
+        //Set the disabled property to FALSE and enable the button.
+        document.getElementById("submit_button").disabled = false;
+    } else{
+        //Otherwise, disable the submit button.
+        document.getElementById("submit_button").disabled = true;
+    }
+}
